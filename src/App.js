9a9dc159aa21188d70ebc8643e361931e6b0b7c5
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-27 15:57:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Basic_class from './Components/Basic_class';
-import Counter from './Components/Counter';
+// import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      {/* <Basic_class name="himanshu"/> */}
+//       <Counter />
+      {<Basic_class name="himanshu"/>}
     </div>
   );
 }
